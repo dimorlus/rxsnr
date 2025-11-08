245cffe42469c279a9105b7aa2517cf2a80484c5
Dmitry Orlov <dimorlus@gmail.com>
2025-11-08 15:36:27 +0200

    08-Nov-25 15:36:27.08

---
+++ cl.cmd
@@ -0,0 +1,11 @@
+"C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MSBuild.exe" rxsnr.vcxproj /t:Clean /p:Configuration=Release /p:Platform=x64
+"C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MSBuild.exe" rxsnr.vcxproj /t:Clean /p:Configuration=Debug /p:Platform=x64
+
+"C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MSBuild.exe" srtmp.vcxproj /t:Clean /p:Configuration=Release /p:Platform=x64
+"C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MSBuild.exe" srtmp.vcxproj /t:Clean /p:Configuration=Debug /p:Platform=x64
+
+"C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MSBuild.exe" rxsnr.vcxproj /t:Clean /p:Configuration=Release /p:Platform=x86
+"C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MSBuild.exe" rxsnr.vcxproj /t:Clean /p:Configuration=Debug /p:Platform=x86
+
+"C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MSBuild.exe" srtmp.vcxproj /t:Clean /p:Configuration=Release /p:Platform=x86
+"C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MSBuild.exe" srtmp.vcxproj /t:Clean /p:Configuration=Debug /p:Platform=x86
--- mk.cmd
+++ mk.cmd
@@ -2,3 +2,7 @@
 pause
 "C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MSBuild.exe" srtmp.vcxproj /p:Configuration=Release /p:Platform=x64
 pause
+"C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MSBuild.exe" rxsnr.vcxproj /p:Configuration=Release /p:Platform=x86
+pause
+"C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MSBuild.exe" srtmp.vcxproj /p:Configuration=Release /p:Platform=x86
+pause
